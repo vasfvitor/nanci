49c1dcbeed405143ae850f381f3b37b0f7ff3345
Ayres Vitor <gitkey@virtuaires.com.br>
2026-06-04 19:41:52 -0300

    prettier

--- desktop/frontend/src/App.vue
+++ desktop/frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'</script>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
 
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
-  <HelloWorld/>
+  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png" />
+  <HelloWorld />
 </template>
 
 <style>
--- desktop/frontend/vite.config.ts
+++ desktop/frontend/vite.config.ts
@@ -7,15 +7,13 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
 
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
-      sassVariables: fileURLToPath(
-        new URL('./src/quasar-variables.sass', import.meta.url)
-      )
-    })
-  ]
+      sassVariables: fileURLToPath(new URL('./src/quasar-variables.sass', import.meta.url)),
+    }),
+  ],
 })
