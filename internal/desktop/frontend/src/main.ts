f0f015160311b4de470bcd376fb6a9f489654477
Ayres Vitor <gitkey@virtuaires.com.br>
2026-06-04 21:19:34 -0300

    fixes

--- internal/desktop/frontend/src/main.ts
+++ internal/desktop/frontend/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import { Quasar, Notify, Dialog } from 'quasar'
 import quasarLang from 'quasar/lang/pt-BR'
-import quasarIconSet from 'quasar/icon-set/material-icons-round'
+import quasarIconSet from 'quasar/icon-set/material-icons'
 
 import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
@@ -18,6 +18,9 @@ myApp.use(Quasar, {
   plugins: {
     Notify,
     Dialog,
+  },
+  config: {
+    dark: 'auto'
   },
   lang: quasarLang,
   iconSet: quasarIconSet,
